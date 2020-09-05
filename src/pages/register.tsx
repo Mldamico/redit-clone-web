7c2771b0cb1693319e602101d938b1106a138b98
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-05 20:39:57 -0300

    Register page

---
+++ src/pages/register.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form, Formik } from 'formik';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from '@chakra-ui/core';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name='username' label='Username' />
+            <Box mt={4}>
+              <InputField name='password' label='Password' type='password' />
+            </Box>
+            <Button
+              type='submit'
+              variantColor='teal'
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Register{' '}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
