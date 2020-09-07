f621cab432623642891527691e0de4273d004471
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-07 10:41:02 -0300

    Registering users

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,17 +4,26 @@ import { Box, Button } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 interface registerProps {}
+import { useRouter } from 'next/router';
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
+
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={async (values) => {
-          console.log(values);
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          console.log(response);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push('/');
+          }
         }}
       >
         {({ isSubmitting }) => (
@@ -29,7 +38,7 @@ const Register: React.FC<registerProps> = ({}) => {
               mt={4}
               isLoading={isSubmitting}
             >
-              Register{' '}
+              Register
             </Button>
           </Form>
         )}
