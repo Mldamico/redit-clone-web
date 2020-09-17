f2a0cc501b18487fb26d45e114dfade19d9edec0
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-17 19:38:00 -0300

    Refactor graphql fragment and added mutation

--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -1,25 +1,31 @@
 import { Box, Button } from '@chakra-ui/core';
 import { Formik, Form } from 'formik';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { InputField } from '../../components/InputField';
 import { Wrapper } from '../../components/Wrapper';
+import { useChangePasswordMutation } from '../../generated/graphql';
 import { toErrorMap } from '../../utils/toErrorMap';
-import login from '../login';
 
 const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+  const router = useRouter();
+  const [, changePassword] = useChangePasswordMutation();
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ newPassword: '' }}
         onSubmit={async (values, { setErrors }) => {
-          // const response = await login(values);
-          // if (response.data?.login.errors) {
-          //   setErrors(toErrorMap(response.data.login.errors));
-          // } else if (response.data?.login.user) {
-          //   // worked
-          //   router.push('/');
-          // }
+          const response = await changePassword({
+            newPassword: values.newPassword,
+            token,
+          });
+          if (response.data?.changePassword.errors) {
+            setErrors(toErrorMap(response.data.changePassword.errors));
+          } else if (response.data?.changePassword.user) {
+            // worked
+            router.push('/');
+          }
         }}
       >
         {({ isSubmitting }) => (
